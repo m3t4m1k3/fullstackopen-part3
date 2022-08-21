83ef45210932f815bcf616cbcf9ccb4be6088946
Michael McGee <github@m3t4m1k3.com>
2022-08-21 10:59:53 +0000

    Troubleshoot vscode port forwarding issues

--- index.js
+++ index.js
@@ -1,34 +1,35 @@
 const express = require("express");
 const cors = require("cors");
-const mongoose = require("mongoose");
-const morgan = require("morgan");
+// const morgan = require("morgan");
+
 require("dotenv").config();
+
 const Person = require("./models/person");
-const { response } = require("express");
+
 const app = express();
 
-morgan.token("body", function (req, res) {
-  return req.method === "POST" ? JSON.stringify(req.body) : "";
-});
+// morgan.token("body", function (req, res) {
+//   return req.method === "POST" ? JSON.stringify(req.body) : "";
+// });
 
 // Middleware
 app.use(express.json());
 app.use(cors());
-app.use(express.static("build"));
-app.use(
-  morgan(":method :url :status :res[content-length] - :response-time ms :body")
-);
+// app.use(express.static("build"));
+// app.use(
+//   morgan(":method :url :status :res[content-length] - :response-time ms :body")
+// );
 
-app.get("/", (req, res) => {
-  res.send("<h1>Phonebook Backend</h1>");
-});
+// app.get("/", (req, res) => {
+//   res.send("<h1>Phonebook Backend</h1>");
+// });
 
-app.get("/info", (req, res) => {
-  res.send(`
-    <p>Phonebook has info for ${persons.length} people</p>
-    <p>${new Date()}</p>
-  `);
-});
+// app.get("/info", (req, res) => {
+//   res.send(`
+//     <p>Phonebook has info for ${persons.length} people</p>
+//     <p>${new Date()}</p>
+//   `);
+// });
 
 app.get("/api/persons", (req, res) => {
   Person.find({}).then((persons) => {
@@ -36,73 +37,74 @@ app.get("/api/persons", (req, res) => {
   });
 });
 
-app.get("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const person = persons.find((person) => person.id === id);
-  if (person) {
-    res.json(person);
-  } else {
-    res.status(404).end();
-  }
-});
+// app.get("/api/persons/:id", (req, res) => {
+//   const id = Number(req.params.id);
+//   const person = persons.find((person) => person.id === id);
+//   if (person) {
+//     res.json(person);
+//   } else {
+//     res.status(404).end();
+//   }
+// });
 
-app.post("/api/persons", (req, res) => {
-  const body = req.body;
+// app.post("/api/persons", (req, res) => {
+//   const body = req.body;
 
-  console.log(body);
+//   console.log(body);
 
-  if (!body.name) {
-    return res.status(400).json({
-      message: "name is missing",
-    });
-  }
+//   if (!body.name) {
+//     return res.status(400).json({
+//       message: "name is missing",
+//     });
+//   }
 
-  if (
-    persons
-      .map((person) => person.name.toLowerCase())
-      .includes(body.name.toLowerCase())
-  ) {
-    return res.status(400).json({
-      message: "name must be unique",
-    });
-  }
+//   if (
+//     persons
+//       .map((person) => person.name.toLowerCase())
+//       .includes(body.name.toLowerCase())
+//   ) {
+//     return res.status(400).json({
+//       message: "name must be unique",
+//     });
+//   }
 
-  if (!body.number) {
-    return res.status(400).json({
-      message: "number is missing",
-    });
-  }
+//   if (!body.number) {
+//     return res.status(400).json({
+//       message: "number is missing",
+//     });
+//   }
 
-  const newPerson = {
-    id: Math.floor(Math.random() * 1000000),
-    name: body.name,
-    number: body.number,
-  };
+//   const newPerson = {
+//     id: Math.floor(Math.random() * 1000000),
+//     name: body.name,
+//     number: body.number,
+//   };
 
-  persons = persons.concat(newPerson);
+//   persons = persons.concat(newPerson);
 
-  res.status(201).json(newPerson);
-});
+//   res.status(201).json(newPerson);
+// });
 
-app.delete("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const deleteIndex = persons.map((person) => person.id).indexOf(id);
+// app.delete("/api/persons/:id", (req, res) => {
+//   const id = Number(req.params.id);
+//   const deleteIndex = persons.map((person) => person.id).indexOf(id);
 
-  if (deleteIndex !== -1) {
-    persons.splice(deleteIndex, 1);
-    res.send({ message: `Person id ${id} removed.` });
-  } else {
-    res.status(404).json({ message: `Person id ${id} not found on server.` });
-  }
-});
+//   if (deleteIndex !== -1) {
+//     persons.splice(deleteIndex, 1);
+//     res.send({ message: `Person id ${id} removed.` });
+//   } else {
+//     res.status(404).json({ message: `Person id ${id} not found on server.` });
+//   }
+// });
 
-const unknownEndpoint = (req, res) => {
-  res.status(404).send({ error: "unknown endpoint" });
-};
+// const unknownEndpoint = (req, res) => {
+//   res.status(404).send({ error: "unknown endpoint" });
+// };
 
-app.use(unknownEndpoint);
+// app.use(unknownEndpoint);
 
-const PORT = process.env.PORT || 3001;
+const PORT = 3001; //process.env.PORT || 3001;
+
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
--- models/person.js
+++ models/person.js
@@ -2,12 +2,12 @@ const mongoose = require("mongoose");
 
 const url = process.env.MONGODB_URI;
 
-console.log("connecting to", url);
+console.log("connecting to MongoDB");
 
 mongoose
   .connect(url)
   .then((result) => {
-    console.log("connected to MongoDB", result);
+    console.log("connected to MongoDB");
   })
   .catch((error) => {
     console.error("error connecting to MongoDB:", error.message);
