6407ec04da769b90a67983c72b23327b9b236d8d
Michael McGee <github@m3t4m1k3.com>
2022-08-22 20:50:15 +0930

    Complete part3 exercise 17

--- index.js
+++ index.js
@@ -1,30 +1,31 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const cors = require("cors");
-const morgan = require("morgan");
-require("dotenv").config();
-const Person = require("./models/person");
+const cors = require('cors');
+const morgan = require('morgan');
+require('dotenv').config();
+const Person = require('./models/person');
+const { update } = require('./models/person');
 
-morgan.token("body", function (req, _res) {
-  return req.method === "POST" ? JSON.stringify(req.body) : "";
+morgan.token('body', function (req, _res) {
+  return req.method === 'POST' ? JSON.stringify(req.body) : '';
 });
 
 // Middleware
 app.use(express.json());
 
 app.use(
-  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+  morgan(':method :url :status :res[content-length] - :response-time ms :body')
 );
 
 app.use(cors());
 
-app.use(express.static("build"));
+app.use(express.static('build'));
 
-app.get("/", (_req, res) => {
-  res.send("<h1>Phonebook Backend</h1>");
+app.get('/', (_req, res) => {
+  res.send('<h1>Phonebook Backend</h1>');
 });
 
-app.get("/info", (_req, res, next) => {
+app.get('/info', (_req, res, next) => {
   Person.find({})
     .then((persons) => {
       res.send(`
@@ -35,24 +36,25 @@ app.get("/info", (_req, res, next) => {
     .catch((error) => next(error));
 });
 
-app.get("/api/persons", (_req, res) => {
+app.get('/api/persons', (_req, res) => {
   Person.find({}).then((persons) => {
     res.json(persons);
   });
 });
 
-app.post("/api/persons", (req, res, next) => {
+// Create
+app.post('/api/persons', (req, res, next) => {
   const { name, number } = req.body;
 
   if (!name) {
     return res.status(400).json({
-      error: "name is missing",
+      error: 'name is missing',
     });
   }
 
   if (!number) {
     return res.status(400).json({
-      error: "number is missing",
+      error: 'number is missing',
     });
   }
 
@@ -64,20 +66,34 @@ app.post("/api/persons", (req, res, next) => {
     .catch((error) => next(error));
 });
 
-app.delete("/api/persons/:id", (req, res) => {
+// Update
+app.put('/api/persons/:id', (request, response, next) => {
+  const { name, number } = request.body;
+  const { id } = request.params;
+  const person = { name, number };
+
+  Person.findByIdAndUpdate(id, person, { new: true })
+    .then((updatedPerson) => {
+      response.json(updatedPerson);
+    })
+    .catch((error) => next(error));
+});
+
+// Delete
+app.delete('/api/persons/:id', (req, res) => {
   Person.findByIdAndRemove(req.params.id)
     .then((deletedPerson) => {
       if (deletedPerson) {
         res.status(204).end();
       } else {
-        res.status(404).send({ error: "id not found in database" });
+        res.status(404).send({ error: 'id not found in database' });
       }
     })
     .catch((error) => next(error));
 });
 
 const unknownEndpoint = (_req, res) => {
-  res.status(404).send({ error: "unknown endpoint" });
+  res.status(404).send({ error: 'unknown endpoint' });
 };
 
 app.use(unknownEndpoint);
@@ -85,8 +101,8 @@ app.use(unknownEndpoint);
 const errorHandler = (error, _request, response, next) => {
   console.error(error.message);
 
-  if (error.name === "CastError") {
-    return response.status(400).send({ error: "malformatted id" });
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' });
   }
 
   next(error);
